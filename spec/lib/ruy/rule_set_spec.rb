a8148bf3c1a927721f700e0458c80683829c4172
Miguel Renom <miguel.renom@moove-it.com>
2014-09-29 12:27:13 -0300

    Remove metadata code

--- spec/lib/ruy/rule_set_spec.rb
+++ spec/lib/ruy/rule_set_spec.rb
@@ -1,29 +1,4 @@
 require 'spec_helper'
 
 describe Ruy::RuleSet do
-
-  describe 'operator overloading' do
-    let(:value) { 'test' }
-
-    describe '#[]' do
-      it 'should return the metadata for given key' do
-        subject.metadata[:test] = value
-
-        expect(subject[:test]).to be == value
-      end
-
-      it 'should return nil if no metadata for given key' do
-         expect(subject[:test]).to be_nil
-      end
-    end
-
-    describe '#[]=' do
-      it 'should set the given value to the given key' do
-        subject[:test] = value
-
-        expect(subject.metadata[:test]).to be == value
-      end
-    end
-
-  end
 end
